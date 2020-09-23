b68fb9343987c08316d5bed143974d5e1592bf99
Ryan <rmbanks@iastate.edu>
2020-09-23 02:17:06 -0500

    Commit Initial Project

---
+++ Unity/Magic-Game/Assets/Voice Command Tests/VoiceCommand.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.Windows.Speech;
+using System;
+
+public class VoiceCommand : MonoBehaviour
+{
+    private KeywordRecognizer keywordRecognizer;
+    private Dictionary<string, Action> actions = new Dictionary<string, Action>();
+
+    private void Start()
+    {
+        actions.Add("forward", Forward);
+        actions.Add("backward", Back);
+        actions.Add("up", Up);
+        actions.Add("down", Down);
+
+        keywordRecognizer = new KeywordRecognizer(actions.Keys.ToArray());
+        keywordRecognizer.OnPhraseRecognized += RecognizedSpeech;
+        keywordRecognizer.Start();
+    }
+
+    private void RecognizedSpeech(PhraseRecognizedEventArgs speech) 
+    {
+        Debug.Log(speech.text);
+        actions[speech.text].Invoke();
+    }
+
+    private void Forward() 
+    {
+        transform.Translate(1, 0, 0);
+    }
+
+    private void Back() 
+    {
+        transform.Translate(-1, 0, 0);
+    }
+
+    private void Up() 
+    {
+        transform.Translate(0, 1, 0);
+    }
+
+    private void Down() 
+    {
+        transform.Translate(0, -1, 0);
+    }
+
+
+}
